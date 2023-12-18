6ff41c948f4e81b29be343a20bd1f7500098a589
Mehedi Hasan Shuvon <mehedi.hasan.shuvon@gmail.com>
2023-12-18 23:42:41 +0600

    hotfix

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -81,7 +81,7 @@ export default function Login({ status, canResetPassword }) {
                         href={route('register')}
                         className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
-                       Don't Have a Account? 
+                       Don't Have an Account? 
                     </Link>
                 </div>
 
